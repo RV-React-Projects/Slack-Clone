e164ff86abb26658a8452c5ca63dc5b6a27f87f7
Ranvijay <ranvijaychouhan12@gmail.com>
2025-06-23 18:59:54 +0530

    Added some more Configs

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,12 @@
-module.exports = {
+/** @type {import("prettier").Config} */
+const prettierConfig = {
+  semi: true,
+  tabWidth: 2,
   arrowParens: 'avoid',
   bracketSameLine: true,
   bracketSpacing: true,
   singleQuote: true,
   trailingComma: 'all',
 };
+
+module.exports = prettierConfig;
